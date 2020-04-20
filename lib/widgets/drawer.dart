fa05081f10d8e3961f1910dd5bc32f6b7f47beff
Ananya <ananyasheshu@gmail.com>
2020-04-20 18:58:36 +0530

    Removed user header

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+// import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../screens/collection_insert_screen.dart';
@@ -12,12 +12,12 @@ import '../screens/profile_screen.dart';
 import '../providers/userAuthentication.dart';
 
 class MainDrawer extends StatelessWidget {
-  final storage = FlutterSecureStorage();
+  // final storage = FlutterSecureStorage();
 
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   SharedPreferences _sharedPreferences;
 
-  void _deleteToken() async{
+  void _deleteToken() async {
     _sharedPreferences = await _prefs;
     _sharedPreferences.remove("token");
     _sharedPreferences.remove("userId");
@@ -26,42 +26,21 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final user = Provider.of<Authentication>(context);
-    final username = user.username;
-    final email = user.email;
-    final profileImageUrl = user.profileImageUrl;
-
     return Drawer(
       child: ListView(
         children: <Widget>[
-          UserAccountsDrawerHeader(
+          DrawerHeader(
             decoration: BoxDecoration(
               gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
                 colors: [
                   Color(0xff191654),
                   Color(0xff43c6ac),
-                  // Color(0xff6dffe1),
-                ]),
-              //color: Theme.of(context).primaryColor,
+                ],
+              ),
             ),
-            // currentAccountPicture: CircleAvatar(
-            //   backgroundColor: Theme.of(context).colorScheme.secondary,
-            //   backgroundImage: (profileImageUrl != null
-            //       ? NetworkImage(profileImageUrl)
-            //       : null),
-            // child: Text(
-            //   username[0].toUpperCase(),
-            //   // "A",
-            //   style: TextStyle(
-            //     fontSize: 30,
-            //     color: Theme.of(context).colorScheme.onSecondary,
-            //   ),
-            // ),
-            // ),
-            accountEmail: Text("johndoe@gmail.com"),
-            accountName: Text("johndoe"),
+            // child: Image.asset('assets/images/logo1.png'),
           ),
           ListTile(
               leading: Icon(Icons.person),
@@ -100,7 +79,7 @@ class MainDrawer extends StatelessWidget {
               trailing: Icon(Icons.cancel),
               onTap: () {
                 _deleteToken();
-                storage.delete(key: "token");
+                // storage.delete(key: "token");
                 Navigator.of(context).pushNamedAndRemoveUntil(
                     '/login-screen', (Route<dynamic> route) => false);
               }),
